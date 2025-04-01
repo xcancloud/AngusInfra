1e2921ea78ecc4ef77277aec1150bffc4a712124
LiuXiaoLong <75962798+xcancloud@users.noreply.github.com>
2025-04-01 11:08:58 +0800

    Set if transport of access token using URI query parameter is supported

--- security/auth-resource-starter/src/main/java/cloud/xcan/angus/security/OAuth2ResourceServerSecurityAutoConfigurer.java
+++ security/auth-resource-starter/src/main/java/cloud/xcan/angus/security/OAuth2ResourceServerSecurityAutoConfigurer.java
@@ -15,6 +15,8 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.oauth2.server.resource.introspection.OpaqueTokenIntrospector;
+import org.springframework.security.oauth2.server.resource.web.BearerTokenResolver;
+import org.springframework.security.oauth2.server.resource.web.DefaultBearerTokenResolver;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.AccessDeniedHandler;
@@ -26,18 +28,27 @@ public class OAuth2ResourceServerSecurityAutoConfigurer {
 
   @Bean
   public SecurityFilterChain resourceServerFilterChain(HttpSecurity http,
-      AccessDeniedHandler accessDeniedHandler, AuthenticationEntryPoint authenticationEntryPoint,
+      BearerTokenResolver bearerTokenResolver, AccessDeniedHandler accessDeniedHandler,
+      AuthenticationEntryPoint authenticationEntryPoint,
       OpaqueTokenIntrospector opaqueTokenIntrospector) throws Exception {
     http.authorizeHttpRequests(authorize -> authorize
         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll() // Allow public access
         .requestMatchers(AUTH_WHITELIST).permitAll() // Allow public access
         .anyRequest().authenticated() // Other requests require authentication
     ).oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer
+        .bearerTokenResolver(bearerTokenResolver)
         .accessDeniedHandler(accessDeniedHandler)
         .authenticationEntryPoint(authenticationEntryPoint)
         .opaqueToken(opaque -> opaque.introspector(opaqueTokenIntrospector))
     ).csrf(AbstractHttpConfigurer::disable); // Disable CSRF protection (configure as needed)
     return http.build();
+  }
+
+  @Bean
+  public BearerTokenResolver bearerTokenResolver() {
+    DefaultBearerTokenResolver resolver = new DefaultBearerTokenResolver();
+    resolver.setAllowUriQueryParameter(true);
+    return resolver;
   }
 
   @Bean

828b2b326458b97cc3883251f5e200118bb97bf2
LiuXiaoLong <75962798+xcancloud@users.noreply.github.com>
2025-04-03 17:06:25 +0800

    Optimise CaffeineCacheBasedUserCache

--- security/auth-server-starter/src/main/java/cloud/xcan/angus/security/authentication/dao/CaffeineCacheBasedUserCache.java
+++ security/auth-server-starter/src/main/java/cloud/xcan/angus/security/authentication/dao/CaffeineCacheBasedUserCache.java
@@ -1,9 +1,14 @@
 package cloud.xcan.angus.security.authentication.dao;
 
+import static cloud.xcan.angus.security.authentication.dao.AbstractUserDetailsAuthenticationProvider.COMPOSITE_ACCOUNT_SEPARATOR;
+import static java.lang.String.format;
+import static org.apache.commons.lang3.ObjectUtils.isNotEmpty;
+
 import cloud.xcan.angus.security.model.CustomOAuth2User;
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.lang.Nullable;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.cache.SpringCacheBasedUserCache;
 
@@ -36,9 +41,26 @@ public class CaffeineCacheBasedUserCache {
     this.cache.put(compositeAccount, user);
   }
 
+  public void putUserInCache(@Nullable String userId, String account, CustomOAuth2User user) {
+    String compositeAccount = getCompositeAccountCacheKey(userId, account);
+    log.debug("Cache put: " + compositeAccount);
+    this.cache.put(compositeAccount, user);
+  }
+
   public void removeUserFromCache(String compositeAccount) {
     log.debug("Cache remove: " + compositeAccount);
     this.cache.invalidate(compositeAccount);
+  }
+
+  public void removeUserFromCache(@Nullable String userId, String account) {
+    String compositeAccount = getCompositeAccountCacheKey(userId, account);
+    log.debug("Cache remove: " + compositeAccount);
+    this.cache.invalidate(compositeAccount);
+  }
+
+  public static String getCompositeAccountCacheKey(String userId, String account) {
+   return isNotEmpty(userId)
+       ? format("%s%s%s", userId, COMPOSITE_ACCOUNT_SEPARATOR, account) : account;
   }
 
 }

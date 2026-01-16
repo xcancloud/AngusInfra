1efdb3980a19412e5d00aa56fa36ce54e3e50e47
LiuXiaoLong <75962798+xcancloud@users.noreply.github.com>
2026-01-16 21:55:25 +0800

    feat: add AuditingVo class for auditing information with schema annotations

---
+++ remote/src/main/java/cloud/xcan/angus/remote/vo/AuditingVo.java
@@ -0,0 +1,30 @@
+package cloud.xcan.angus.remote.vo;
+
+import cloud.xcan.angus.remote.NameJoinField;
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.time.LocalDateTime;
+import lombok.Data;
+
+@Data
+public class AuditingVo {
+
+  @Schema(description = "创建者ID")
+  private Long createdBy;
+
+  @Schema(description = "创建者姓名")
+  @NameJoinField(id = "createdBy", repository = "commonUserBaseRepo")
+  private String creator;
+
+  @Schema(description = "创建时间")
+  private LocalDateTime createdDate;
+
+  @Schema(description = "最后修改人ID")
+  private Long modifiedBy;
+
+  @NameJoinField(id = "modifiedBy", repository = "commonUserBaseRepo")
+  private String modifier;
+
+  @Schema(description = "最后修改时间")
+  private LocalDateTime modifiedDate;
+
+}

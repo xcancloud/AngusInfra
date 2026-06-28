9266a7b25ba61837ff5c2ffa4c93475168de15d9
LiuXiaoLong <75962798+xcancloud@users.noreply.github.com>
2026-06-28 14:20:05 +0800

    feat: Remove deprecated OpenAPI model classes to streamline the codebase and improve maintainability

--- angus-infra-spec/src/main/java/io/swagger/v3/oas/models/SpecVersion.java
+++
@@ -1,6 +0,0 @@
-package io.swagger.v3.oas.models;
-
-public enum SpecVersion {
-  V30,
-  V31
-}
--- angus-infra-spec/src/main/java/io/swagger/v3/oas/models/annotations/OpenAPI30.java
+++
@@ -1,8 +0,0 @@
-package io.swagger.v3.oas.models.annotations;
-
-import java.lang.annotation.Inherited;
-
-@Inherited
-public @interface OpenAPI30 {
-
-}
